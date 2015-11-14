b9315abc5292d22d4e988c521521b1d1b2fd07dd
Daniel del Río <dandelrr@gmail.com>
2015-11-14 16:16:46 +0100

    Solución reto week 2 completo

--- JuegodeMemoria.playground/Contents.swift
+++ JuegodeMemoria.playground/Contents.swift
@@ -1,8 +1,18 @@
-//: Playground - noun: a place where people can play
+//: Juego de Memoria - Daniel del Río
 
 import UIKit
 
-var str = "Hello, playground"
-
-var hola = "hola"
-
+for numero in 0...100 {
+    if numero % 5 == 0 {
+        print("\(numero)" + " Bingo!!!")
+    }
+    if numero % 2 == 0 {
+        print ("\(numero)" + " par!!!")
+    }
+    else {
+        print("\(numero)" + " impar!!!")
+    }
+    if numero >= 30 && numero <= 40 {
+        print("\(numero)" + " Viva Swift!!!")
+    }
+}
